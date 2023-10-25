6b80cae12dbe502e4620ee86f186bdb34627c6bf
117503445 <t117503445@gmail.com>
2023-10-25 20:25:03 +0800

    feat: api
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- peer-discovery/src/main.rs
+++ peer-discovery/src/main.rs
@@ -1,28 +1,45 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize)]
+struct CommonResponse<T> {
+    code: i32,
+    msg: String,
+    data: T,
+}
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn index() -> impl Responder {
+    HttpResponse::Ok().json(CommonResponse {
+        code: 0,
+        msg: "".to_string(),
+        data: "",
+    })
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+#[derive(Deserialize)]
+struct HeartbeatRequest {
+    ip: String,
+    peer_id: String,
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[post("/api/heartbeat")]
+async fn heartbeat(req_body: web::Json<HeartbeatRequest>) -> impl Responder {
+    
+    HttpResponse::Ok().json(CommonResponse {
+        code: 0,
+        msg: "".to_string(),
+        data: "",
+    })
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    let port = 8080;
+    println!("Starting server on port {}", port);
+
+    HttpServer::new(|| App::new().service(index))
+        .bind(("0.0.0.0", port))?
+        .run()
+        .await
 }
