5a7bbb8040b50074d9c05e82dbbd2f77bb8211a4
117503445 <t117503445@gmail.com>
2023-11-06 20:20:45 +0800

    feat: fe
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- src/uni-drop/src/App.tsx
+++ src/uni-drop/src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import settingImg from './assets/setting.svg'
+
 
 function App() {
   return (
@@ -22,10 +24,16 @@ function App() {
               <span className="font-bold">Uni</span> versal Air<span className="font-bold">Drop</span>.
             </span>
 
-            
+
             <div className="flex flex-col">
               <div className="h-[4rem] py-2 my-1.5 bg-white w-[16rem] rounded-md flex mx-auto shadow-md"></div>
               <div className="h-[4rem] py-2 my-1.5 bg-white w-[16rem] rounded-md flex mx-auto shadow-md"></div>
+            </div>
+
+            <div className="flex-1 flex max-h-[5rem] mt-auto">
+              <button className="h-[2rem] w-[2rem] rounded-md bg-white shadow-md flex justify-center items-center">
+                <img src={settingImg}></img>
+              </button>
             </div>
 
           </div>
@@ -34,7 +42,8 @@ function App() {
           <div className="w-full h-full flex flex-col">
             <div className="h-[3.75rem] w-full flex justify-between items-center px-5 border-b-2"></div>
             <div className="flex-1 w-full flex justify-center items-center"></div>
-            <div className="h-[8rem] w-full flex justify-between items-center px-5 border-t-2"></div>
+            <div className="h-[8rem] w-full flex justify-between items-center px-5 border-t-2">
+          </div>
 
 
           </div>
