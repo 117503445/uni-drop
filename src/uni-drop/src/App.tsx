26cbbc05a1bf459ac068fbceaafcf5cd0dc4c20c
117503445 <t117503445@gmail.com>
2023-11-06 17:28:11 +0800

    feat: fe
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- src/uni-drop/src/App.tsx
+++ src/uni-drop/src/App.tsx
@@ -14,10 +14,9 @@ function App() {
       </button>
 
       <div className="h-screen w-screen flex justify-center items-center">
-        <div className="rounded-md w-[calc(100%-5rem)] max-w-[75] h-[calc(100%-5rem)] border-2 shadow-md flex">
+        <div className="rounded-[1rem] w-[calc(100%-5rem)] max-w-[75rem] h-[calc(100%-5rem)] border-2 shadow-md flex overflow-hidden">
           {/* left side */}
           <div className="w-[18.75rem] h-full bg-[#DCECF3] shadow-md flex flex-col">
-            {/* header */}
             <div className="h-[3.75rem] w-full flex flex-col justify-between items-center px-5 border-b-2">
               <span className="text-xl font-bold">UniDrop</span>
               <span className="text-xl ">Universal AirDrop.</span>
@@ -25,22 +24,13 @@ function App() {
           </div>
 
           {/* right side */}
-          <div className="w-[calc(100%-18.75rem)] h-full flex flex-col">
-            {/* header */}
+          <div className="w-full h-full flex flex-col">
             <div className="h-[3.75rem] w-full flex justify-between items-center px-5 border-b-2"></div>
-
-            {/* body */}
-            <div className="flex-1 w-full flex justify-center items-center">
-              {/* <div className='w-[calc(100%-10rem)] h-[calc(100%-10rem)] bg-white rounded-md shadow-md flex flex-col justify-center items-center'>
-                                <div className='text-2xl font-bold'>Welcome</div>
-                                <div className='text-xl'>to</div>
-                                <div className='text-2xl font-bold'>React Tailwind</div>
-                            </div> */}
-            </div>
-
-            {/* footer */}
+            <div className="flex-1 w-full flex justify-center items-center"></div>
             <div className="h-[8rem] w-full flex justify-between items-center px-5 border-t-2"></div>
           </div>
+
+
         </div>
       </div>
     </div>
