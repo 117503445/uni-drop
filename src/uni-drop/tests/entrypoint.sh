9f199ff054b9871c301cf8817813afed571f0dc3
117503445 <t117503445@gmail.com>
2023-11-23 00:10:38 +0800

    feat: e2e novnc
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- src/uni-drop/tests/entrypoint.sh
+++ src/uni-drop/tests/entrypoint.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env bash
 
-rm -f /workspace/*.log
+rm -f /workspace/logs/*.log
+
+export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
 
 # Start Xvfb
-export DISPLAY=:1
-Xvfb :1 -screen 0 1024x768x16 > /workspace/xvfb.stdout.log 2>&1 &
-sleep 5
-fluxbox > /workspace/fluxbox.stdout.log 2>&1 &
+Xvfb $DISPLAY -screen 0 $GEOMETRY -ac +extension RANDR > /workspace/xvfb.stdout.log 2>&1 &
+
+# wait for Xvfb to start
+# for i in $(seq 1 10); do
+#  xdpyinfo -display $DISPLAY >/dev/null 2>&1
+#  if [ $? -eq 0 ]; then
+#    break
+#  fi
+#  echo Waiting xvfb...
+#  sleep 1
+# done
+# echo xvfb start success
+
+# Start fluxbox
+fluxbox -display $DISPLAY > /workspace/fluxbox.stdout.log 2>&1 &
 
 # start x11
-x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever > /workspace/x11vnc.stdout.log 2>&1 &
+x11vnc -display $DISPLAY -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever > /workspace/x11vnc.stdout.log 2>&1 &
 
 # Start noVNC
-/workspace/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen localhost:9001 > /workspace/noVNC.stdout.log 2>&1 &
+/workspace/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen localhost:9900 > /workspace/noVNC.stdout.log 2>&1 &
 
+bash
