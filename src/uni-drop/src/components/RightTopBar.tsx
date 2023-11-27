48c093c2408ee41b7c5859f7d5f5199823dfcc93
117503445 <t117503445@gmail.com>
2023-11-27 23:36:48 +0800

    feat: RightTopBar
    Signed-off-by: 117503445 <t117503445@gmail.com>

---
+++ src/uni-drop/src/components/RightTopBar.tsx
@@ -0,0 +1,18 @@
+import returnIcon from "@/assets/return.svg";
+
+export default function RightTopBar(props: {children: JSX.Element | never[] }  ) {
+  return (
+    <div className="flex h-[3.75rem] w-full flex-row items-center justify-between border-b-2 px-5">
+      {/* <span>{idToName(props.selectedPeerID)}</span> */}
+      {props.children}
+      <button
+        className="flex h-[1.5rem] w-[2.25rem] items-center justify-center rounded-xl bg-white fill-none shadow-md sm:hidden"
+        onClick={() => {
+          window.location.hash = "/";
+        }}
+      >
+        <img className="mx-2" src={returnIcon}></img>
+      </button>
+    </div>
+  );
+}
