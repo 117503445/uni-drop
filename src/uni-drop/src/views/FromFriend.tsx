0a5c546e03a9e8220fe5d469d489cd9e660a0a28
117503445 <t117503445@gmail.com>
2023-11-27 21:01:29 +0800

    pref: lazy load
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- src/uni-drop/src/views/FromFriend.tsx
+++ src/uni-drop/src/views/FromFriend.tsx
@@ -5,7 +5,7 @@ export default function FromFriend(props: {
   addPeer: (peerId: string) => void;
 }) {
   useEffect(() => {
-    // TODO: this is a hack, we should use a better way to pass peerID
+    // TODO: timeout is a hack, we should use a better way to pass peerID
     setTimeout(() => {
       const peerID = window.location.href.split("/").pop();
       console.log("FromFriend peerID", peerID);
@@ -13,7 +13,7 @@ export default function FromFriend(props: {
         props.addPeer(peerID);
       }
       window.location.hash = `/chat/${peerID}`;
-    }, 1000);
+    }, 500);
   }, [props]);
 
   return <div>From friend</div>;
