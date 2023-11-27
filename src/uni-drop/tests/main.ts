581f7387dab2f879f22723aca31aba91d152ae49
117503445 <t117503445@gmail.com>
2023-11-27 13:47:30 +0800

    refactor: tests
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- src/uni-drop/tests/main.ts
+++ src/uni-drop/tests/main.ts
@@ -1,6 +1,7 @@
-import { Browser, chromium } from "playwright";
+import { Browser, Page, chromium } from "playwright";
 import { program } from "commander";
 import * as fs from "fs";
+import { exit } from "process";
 
 program.option("--url <url>", "the url of frontend", "http://localhost:5173");
 program.parse();
@@ -24,50 +25,72 @@ class TestCase {
   constructor(
     public name: string,
     public func: (browser: Browser) => Promise<void>,
+    private config: Map<string, string> = new Map(),
     public timeout: number = 10000,
   ) {}
+
   async run() {
     const browser = await chromium.launch({
       headless: false,
     });
     try {
       console.log(`Running test case: ${this.name}`);
+
+      let configText = "";
+      for (const [key, value] of this.config) {
+        configText += `${key}=${value}\n`;
+      }
+      await fs.promises.writeFile(".env.development.local", configText);
+
       await this.func(browser);
       console.log(`Test case ${this.name} passed`);
     } catch (error) {
       console.log(`Test case ${this.name} failed`);
       console.log(error);
+      exit(1);
     } finally {
       browser.close();
     }
   }
 }
 
-async function testBasic(browser: Browser) {
-  await fs.promises.writeFile(".env.development.local", "");
+async function getInnerText(page: Page, selector: string) {
+  for (let i = 0; i < 10; i++) {
+    const element = page.locator(selector);
+    if (element) {
+      return element.innerText();
+    }
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+  throw new Error(`Could not get innerText of ${selector}`);
+}
 
+async function getPeerName(page: Page) {
+  return getInnerText(page, '//*[@id="peerName"]');
+}
+
+async function getPeerID(page: Page) {
+  return getInnerText(page, '//*[@id="peerID"]');
+}
+
+async function sendMsg(page: Page, msg: string) {
+  await page.getByPlaceholder("Type message here").click();
+  await page.getByPlaceholder("Type message here").fill(msg);
+  await page.keyboard.press("Enter");
+}
+
+async function testBasic(browser: Browser) {
   const context = await browser.newContext();
 
   const page1 = await context.newPage();
   const page2 = await context.newPage();
-
   await Promise.all([page1.goto(url), page2.goto(url)]);
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 
-  let page1Name = "";
-  let page2Name = "";
-
-  for (let i = 0; i < 10; i++) {
-    const [id1, id2] = await Promise.all([
-      page1.locator('//*[@id="peerName"]').innerText(),
-      page2.locator('//*[@id="peerName"]').innerText(),
-    ]);
-    if (id1 !== "" && id2 !== "") {
-      page1Name = id1;
-      page2Name = id2;
-      break;
-    }
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-  }
+  const [page1Name, page2Name] = await Promise.all([
+    getPeerName(page1),
+    getPeerName(page2),
+  ]);
 
   if (page1Name === "" || page2Name === "") {
     throw new Error("Could not get peer Names");
@@ -81,48 +104,28 @@ async function testBasic(browser: Browser) {
   ]);
 
   const msg1 = "Hello";
-  await page1.getByPlaceholder("Type message here").click();
-  await page1.getByPlaceholder("Type message here").fill(msg1);
-  await page1.keyboard.press("Enter");
+  await sendMsg(page1, msg1);
   await new Promise((resolve) => setTimeout(resolve, 1000));
   assertOne(await page2.getByText(msg1, { exact: true }).all());
 
   const msg2 = "Hi";
-  await page2.getByPlaceholder("Type message here").click();
-  await page2.getByPlaceholder("Type message here").fill(msg2);
-  await page2.keyboard.press("Enter");
+  await sendMsg(page2, msg2);
   await new Promise((resolve) => setTimeout(resolve, 1000));
   assertOne(await page1.getByText(msg2, { exact: true }).all());
 }
 
 async function testAddPeerID(browser: Browser) {
-  await fs.promises.writeFile(
-    ".env.development.local",
-    "VITE_DISABLE_HEARTBEAT=true",
-  );
-
   const context = await browser.newContext();
 
   const page1 = await context.newPage();
   const page2 = await context.newPage();
-
   await Promise.all([page1.goto(url), page2.goto(url)]);
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 
-  let page1Id = "";
-  let page2Id = "";
-
-  for (let i = 0; i < 10; i++) {
-    const [id1, id2] = await Promise.all([
-      page1.locator('//*[@id="peerID"]').innerText(),
-      page2.locator('//*[@id="peerID"]').innerText(),
-    ]);
-    if (id1 !== "" && id2 !== "") {
-      page1Id = id1;
-      page2Id = id2;
-      break;
-    }
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-  }
+  const [page1Id, page2Id] = await Promise.all([
+    getPeerID(page1),
+    getPeerID(page2),
+  ]);
 
   if (page1Id === "" || page2Id === "") {
     throw new Error("Could not get peer IDs");
@@ -135,22 +138,12 @@ async function testAddPeerID(browser: Browser) {
   await page1.getByPlaceholder("Add friend by peer id").fill(page2Id);
   await page1.getByText("AddFriendByID").click();
 
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 
-  let page1Name = "";
-  let page2Name = "";
-  for (let i = 0; i < 10; i++) {
-    const [name1, name2] = await Promise.all([
-      page1.locator('//*[@id="peerName"]').innerText(),
-      page2.locator('//*[@id="peerName"]').innerText(),
-    ]);
-    if (name1 !== "" && name2 !== "") {
-      page1Name = name1;
-      page2Name = name2;
-      break;
-    }
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-  }
+  const [page1Name, page2Name] = await Promise.all([
+    getPeerName(page1),
+    getPeerName(page2),
+  ]);
 
   if (page1Name === "" || page2Name === "") {
     throw new Error("Could not get peer Names");
@@ -164,16 +157,12 @@ async function testAddPeerID(browser: Browser) {
   ]);
 
   const msg1 = "Hello";
-  await page1.getByPlaceholder("Type message here").click();
-  await page1.getByPlaceholder("Type message here").fill(msg1);
-  await page1.keyboard.press("Enter");
+  await sendMsg(page1, msg1);
   await new Promise((resolve) => setTimeout(resolve, 1000));
   assertOne(await page2.getByText(msg1, { exact: true }).all());
 
   const msg2 = "Hi";
-  await page2.getByPlaceholder("Type message here").click();
-  await page2.getByPlaceholder("Type message here").fill(msg2);
-  await page2.keyboard.press("Enter");
+  await sendMsg(page2, msg2);
   await new Promise((resolve) => setTimeout(resolve, 1000));
   assertOne(await page1.getByText(msg2, { exact: true }).all());
 }
@@ -192,7 +181,11 @@ async function testAddPeerID(browser: Browser) {
 
   const cases = [
     new TestCase("Basic", testBasic),
-    new TestCase("AddPeerID", testAddPeerID),
+    new TestCase(
+      "AddPeerID",
+      testAddPeerID,
+      new Map([["VITE_DISABLE_HEARTBEAT", "true"]]),
+    ),
   ];
   for (const c of cases) {
     await c.run();
